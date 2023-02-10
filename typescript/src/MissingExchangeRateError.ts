46db34e6ff4b73f2868bb70503a7e28e91fc8c47
Julien VITTE <julien.vitte@insidegroup.fr>
2023-02-10 16:23:29 +0100

    feat(typescript): adds typescript kata

---
+++ typescript/src/MissingExchangeRateError.ts
@@ -0,0 +1,9 @@
+import { Currency } from './Currency'
+
+export class MissingExchangeRateError extends Error {
+  constructor (currency1: Currency, currency2: Currency) {
+    super(currency1 + '-> ' + currency2)
+  }
+
+  message: string
+}
---
+++ typescript/tests/MoneyCalculator.spec.ts
@@ -0,0 +1,17 @@
+import { Currency } from '../src/Currency'
+import { MoneyCalculator } from '../src/MoneyCalculator'
+
+describe('Money', function () {
+  test('add in usd returns number', () => {
+    expect(MoneyCalculator.Add(5, Currency.USD, 10)).toBeNumber()
+    expect(MoneyCalculator.Add(5, Currency.USD, 10)).not.toBeNull()
+  })
+
+  test('multiply in eur returns positive number', () => {
+    expect(MoneyCalculator.Times(10, Currency.EUR, 2)).toBeGreaterThan(0)
+  })
+
+  test('divide in korean won returns number', () => {
+    expect(1000.5, ).toBe(MoneyCalculator.Divide(4002, Currency.KRW, 4))
+  })
+})
