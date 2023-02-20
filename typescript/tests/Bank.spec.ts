b50a472afa4ceae5d1f356ae8a3f80a992939165
Guillaume <guillaume.froidcourt@etu.u-bordeaux.fr>
2023-02-20 16:20:21 +0100

    :white_check_mark: Test refactors

--- typescript/tests/Bank.spec.ts
+++ typescript/tests/Bank.spec.ts
@@ -3,24 +3,38 @@ import { Bank } from '../src/Bank'
 import { MissingExchangeRateError } from '../src/MissingExchangeRateError'
 
 describe('Bank', function () {
-  test('convert from eur to usd returns number', () => {
-    expect(Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2).Convert(10, Currency.EUR, Currency.USD)).toBe(12)
+  test('Should return number when converting from eur to usd', () => {
+    // Given
+    const bank: Bank = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2)
+    // When
+    const actual: number = bank.Convert(10, Currency.EUR, Currency.USD)
+    // Then
+    expect(actual).toBe(12)
   })
 
-  test('convert from usd to usd returns same value', () => {
-    expect(Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2).Convert(10, Currency.EUR, Currency.EUR)).toBe(10)
+  test('Should return the same value when converting from usd to usd', () => {
+    const bank: Bank = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2)
+    const actual: number = bank.Convert(10, Currency.USD, Currency.USD)
+    expect(actual).toBe(10)
   })
 
-  test('convert throws error in case of missing exchange rates', () => {
-    expect(() => Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2).Convert(10, Currency.EUR, Currency.KRW))
-      .toThrow(MissingExchangeRateError)
+  test('Should throw error in case of missing exchange rates', () => {
+    const bank: Bank = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2)
+    // When - Then
+    expect(() => bank.Convert(10, Currency.EUR, Currency.KRW)).toThrow(MissingExchangeRateError)
   })
 
-  test('convert with different exchange rates returns different numbers', () => {
-    expect(Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2).Convert(10, Currency.EUR, Currency.USD)).toBe(12)
+  test('Should return different numbers when converting with different exchange rates', () => {
+    let bank: Bank = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2)
+    let actual: number = bank.Convert(10, Currency.EUR, Currency.USD)
+    expect(actual).toBe(12)
 
-    expect(Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.3).Convert(10, Currency.EUR, Currency.USD)).toBe(13)
+    bank = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.3)
+    actual = bank.Convert(10, Currency.EUR, Currency.USD)
+    expect(actual).toBe(13)
 
-    expect(Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.5).Convert(10, Currency.EUR, Currency.USD)).toBe(15)
+    bank = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.5)
+    actual = bank.Convert(10, Currency.EUR, Currency.USD)
+    expect(actual).toBe(15)
   })
 })
--- typescript/tests/MoneyCalculator.spec.ts
+++ typescript/tests/MoneyCalculator.spec.ts
@@ -2,16 +2,19 @@ import { Currency } from '../src/Currency'
 import { MoneyCalculator } from '../src/MoneyCalculator'
 
 describe('Money', function () {
+  const money: number = MoneyCalculator.Add(5, Currency.USD, 10)
   test('add in usd returns number', () => {
-    expect(MoneyCalculator.Add(5, Currency.USD, 10)).toBeNumber()
-    expect(MoneyCalculator.Add(5, Currency.USD, 10)).not.toBeNull()
+    expect(money).toBeNumber()
+    expect(money).not.toBe(null)
   })
 
   test('multiply in eur returns positive number', () => {
-    expect(MoneyCalculator.Times(10, Currency.EUR, 2)).toBeGreaterThan(0)
+    const money: number = MoneyCalculator.Times(10, Currency.EUR, 2)
+    expect(money).toBeGreaterThan(0)
   })
 
   test('divide in korean won returns number', () => {
-    expect(1000.5).toBe(MoneyCalculator.Divide(4002, Currency.KRW, 4))
+    const money: number = MoneyCalculator.Divide(4002, Currency.KRW, 4)
+    expect(money).toBe(1000.5)
   })
 })
