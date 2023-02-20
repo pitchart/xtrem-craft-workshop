8e91d679c8e071961e8f479427bb642ff90baea8
TripleLardon <bardinet.andrea@gmail.com>
2023-02-20 16:43:56 +0100

    refactor tests bank

--- typescript/tests/Bank.spec.ts
+++ typescript/tests/Bank.spec.ts
@@ -3,24 +3,33 @@ import { Bank } from '../src/Bank'
 import { MissingExchangeRateError } from '../src/MissingExchangeRateError'
 
 describe('Bank', function () {
-  test('convert from eur to usd returns number', () => {
-    expect(Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2).Convert(10, Currency.EUR, Currency.USD)).toBe(12)
+
+  let bank;
+
+  beforeEach(() => {
+    bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2)
   })
 
-  test('convert from usd to usd returns same value', () => {
-    expect(Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2).Convert(10, Currency.EUR, Currency.EUR)).toBe(10)
+
+  test(`convert EUR to USD return number`, () => {
+    expect(bank.Convert(10, Currency.EUR, Currency.USD)).toBe(12)
+  })
+
+  test(`convert USD to EUR return same number`, () => {
+    expect(bank.Convert(10, Currency.EUR, Currency.EUR)).toBe(10)
   })
 
   test('convert throws error in case of missing exchange rates', () => {
-    expect(() => Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2).Convert(10, Currency.EUR, Currency.KRW))
+    expect(() => bank.Convert(10, Currency.EUR, Currency.KRW))
       .toThrow(MissingExchangeRateError)
+    // expect(() =>  bank.Convert(10, Currency.EUR, Currency.KRW )).toThrow(MissingExchangeRateError)
   })
 
-  test('convert with different exchange rates returns different numbers', () => {
-    expect(Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2).Convert(10, Currency.EUR, Currency.USD)).toBe(12)
-
-    expect(Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.3).Convert(10, Currency.EUR, Currency.USD)).toBe(13)
-
-    expect(Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.5).Convert(10, Currency.EUR, Currency.USD)).toBe(15)
+  test(`convert with different exchange rates returns different numbers`, () => {
+    bank.AddExchangeRate(Currency.USD, Currency.EUR, 1.3)
+    expect(bank.Convert(10, Currency.USD, Currency.EUR)
+    ).toBe(13)
   })
+
+
 })
