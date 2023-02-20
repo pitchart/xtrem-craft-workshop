2e50ee1e0e2d351aa11770731523b3fb759dd8a9
valmri <valmarmie87@outlook.com>
2023-02-20 16:23:38 +0100

    [4] Tests unitaires

--- php/tests/MoneyProblem/MoneyTest.php
+++ php/tests/MoneyProblem/MoneyTest.php
@@ -10,22 +10,37 @@ class MoneyTest extends TestCase
 {
     public function test_add_returns_value()
     {
-        $this->assertIsFloat(MoneyCalculator::add(5, 10));
-        $this->assertNotNull(MoneyCalculator::add(5, 10));
-        $this->assertEquals(MoneyCalculator::add(5,5),10);
+        // Arrange & Act
+        $add = MoneyCalculator::add(5, 10);
+        $add2 = MoneyCalculator::add(5,5);
+
+        // Assert
+        $this->assertIsFloat($add);
+        $this->assertNotNull($add);
+        $this->assertEquals($add2,10);
     }
 
     public function test_multiply_returns_positive_number()
     {
-        $this->assertLessThan(MoneyCalculator::times(10, 2), 0);
-        $this->assertEquals(MoneyCalculator::times(5,2),10);
-        $this->greaterThan(MoneyCalculator::times(5,2), 5);
+        // Arrange & Act
+        $times = MoneyCalculator::times(5, 2);
+        $times2 = MoneyCalculator::times(10, 2);
+
+        // Assert
+        $this->assertLessThan($times2, 0);
+        $this->assertEquals($times,10);
+        $this->greaterThan($times, 5);
     }
 
     public function test_divide_won_returns_float()
     {
-        $this->assertEquals(MoneyCalculator::divide(4002, 4), 1000.5);
-        $this->lessThan(MoneyCalculator::divide(5,2), 3);
-        $this->greaterThan(MoneyCalculator::divide(5,2), 1);
+        // Arrange & Act
+        $divide = MoneyCalculator::divide(5, 2);
+        $divide2 = MoneyCalculator::divide(4002, 4);
+
+        // Assert
+        $this->assertEquals($divide2, 1000.5);
+        $this->lessThan($divide, 3);
+        $this->greaterThan($divide, 1);
     }
 }
