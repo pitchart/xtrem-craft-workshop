c7ae5ab4ff01de97c931ce7cfa2f3084fac8c8b1
augcolas <augus.colas@orange.fr>
2023-03-02 14:50:07 +0100

    Updating tests

--- typescript/tests/Bank.spec.ts
+++ typescript/tests/Bank.spec.ts
@@ -4,12 +4,16 @@ import { MissingExchangeRateError } from '../src/MissingExchangeRateError'
 
 describe('Bank', function () {
 
+  let bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2);
+
   test('convert from eur to usd returns number', () => {
-    expect(Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2).Convert(10, Currency.EUR, Currency.USD)).toBe(12)
+    let converted = bank.Convert(10, Currency.EUR, Currency.USD);
+    expect(converted).toBe(12);
   })
 
   test('convert from usd to usd returns same value', () => {
-    expect(Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2).Convert(10, Currency.EUR, Currency.EUR)).toBe(10)
+    let converted = bank.Convert(10, Currency.EUR, Currency.EUR);
+    expect(converted).toBe(10);
   })
 
   test('convert throws error in case of missing exchange rates', () => {
