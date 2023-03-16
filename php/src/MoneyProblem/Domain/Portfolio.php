2dd7e85f6b156b363081de10a558f7754f0af18d
Gregory SIMON <simon.24260@gmail.com>
2023-03-16 17:34:00 +0100

    Refacto Portfolio

--- php/src/MoneyProblem/Domain/Portfolio.php
+++ php/src/MoneyProblem/Domain/Portfolio.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace MoneyProblem\Domain;
-use MoneyProblem\Domain\MoneyCalculator;
 
 class Portfolio {
 
@@ -14,26 +13,56 @@ class Portfolio {
         $this->wallet = [];
     }
 
-    public function addMoney(Currency $currency, $amount){
-        if(!array_key_exists($currency->getValue(), $this->wallet)) {
-            $this->wallet[$currency->getValue()] = $amount;
-         } else {
-            $this->wallet[$currency->getValue()] += $amount;
+    public function addMoney( Money $money){
+        if(empty($this->wallet)){
+            array_push($this->wallet,$money);
+            return ;
         }
+        $currency = $money->getCurrency()->getValue();
+        $find = false;
+        $index = 0;
+        while(!$find && $index < count($this->wallet)){
+            if($this->wallet[$index]->getCurrency()->getValue() == $currency){
+                $find = true;
+                $newAmount = $this->wallet[$index]->getAmount() + $money->getAmount();
+                $this->wallet[$index]->setAmount($newAmount);
+                return ;
+            }
+            $index++;
+        }
+        if(!$find){
+            array_push($this->wallet,$money);
+            return ;
+        }
+    }
+
+    public function sumOld(Currency $currency){
+        $sum = 0;
+        foreach ($this->wallet as $walletCurrency => $amount) {
+            $sum += $this->bank->convert($amount,Currency::from($walletCurrency), $currency);
+        }
+        return $sum;
     }
 
     public function sum(Currency $currency){
         $sum = 0;
-        foreach ($this->wallet as $walletCurrency => $amount) {
-            $sum += $this->bank->convertOld($amount,Currency::from($walletCurrency), $currency);
+        foreach ($this->wallet as $money) {
+            $sum += $this->bank->convert($money, $currency);
         }
         return $sum;
     }
 
     public function getAmountByCurrency(Currency $currency){
+
+        $find = false;
+        $index = 0;
         $amount = 0;
-        if(array_key_exists($currency->getValue(), $this->wallet)){
-            $amount = $this->wallet[$currency->getValue()];
+        while(!$find && $index < count($this->wallet)){
+            if($this->wallet[$index]->getCurrency()->getValue() == $currency){
+                $find = true;
+                $amount = $this->wallet[$index]->getAmount();
+            }
+            $index++;
         }
         return $amount;
     }
--- php/tests/MoneyProblem/PortfolioTest.php
+++ php/tests/MoneyProblem/PortfolioTest.php
@@ -3,26 +3,30 @@ namespace Tests\MoneyProblem;
 
 use MoneyProblem\Domain\Bank;
 use MoneyProblem\Domain\Currency;
-use MoneyProblem\Domain\MoneyCalculator;
+use MoneyProblem\Domain\Money;
 use MoneyProblem\Domain\Portfolio;
 use PHPUnit\Framework\TestCase;
-use Pitchart\Transformer\Curry;
 
 class PortfolioTest extends TestCase
 {
     public function test_sum_potfolio() {
         $bank = Bank::create(Currency::EUR(), Currency::USD(), 1.2);
         $portfolio = new Portfolio($bank);
-        $portfolio->addMoney(Currency::USD(), 5);
-        $portfolio->addMoney(Currency::EUR(), 10);
+        $money = new Money(5,Currency::USD());
+        $money2 = new Money(10,Currency::EUR()); 
+        $portfolio->addMoney($money);
+        $portfolio->addMoney($money2);
         $sum = $portfolio->sum(Currency::USD());
         $this->assertEquals(17, $sum);
     }
+    
     public function test_add_potfolio() {
         $bank = Bank::create(Currency::EUR(), Currency::USD(), 1.2);
         $portfolio = new Portfolio($bank);
-        $portfolio->addMoney(Currency::EUR(), 5);
-        $portfolio->addMoney(Currency::EUR(), 5);
+        $money = new Money(5,Currency::EUR());
+        $money2 = new Money(5,Currency::EUR()); 
+        $portfolio->addMoney($money);
+        $portfolio->addMoney($money2);
         $amountEUR = $portfolio->getAmountByCurrency(Currency::EUR());
         $amountUSD = $portfolio->getAmountByCurrency(Currency::USD());
         $this->assertEquals(0, $amountUSD);
@@ -33,17 +37,21 @@ class PortfolioTest extends TestCase
         $bank = Bank::create(Currency::EUR(), Currency::KRW(), 1344);
         $bank->addEchangeRate(Currency::USD(), Currency::KRW(), 1100);
         $portfolio = new Portfolio($bank);
-        $portfolio->addMoney(Currency::USD(), 5);
-        $portfolio->addMoney(Currency::EUR(), 10);
+        $money = new Money(5,Currency::USD());
+        $money2 = new Money(10,Currency::EUR()); 
+        $portfolio->addMoney($money);
+        $portfolio->addMoney($money2);
         $sum = $portfolio->sum(Currency::KRW());
         $this->assertEquals(18940, $sum);
     }
 
     public function test_convert2_potfolio() {
         $bank = Bank::create(Currency::USD(), Currency::KRW(), 1100);
+        $money = new Money(1,Currency::USD());
+        $money2 = new Money(1100,Currency::KRW()); 
         $portfolio = new Portfolio($bank);
-        $portfolio->addMoney(Currency::USD(), 1);
-        $portfolio->addMoney(Currency::KRW(), 1100);
+        $portfolio->addMoney($money);
+        $portfolio->addMoney($money2);
         $sum = $portfolio->sum(Currency::KRW());
         $this->assertEquals(2200, $sum);
     }
@@ -51,9 +59,12 @@ class PortfolioTest extends TestCase
     public function test_sum2_potfolio() {
         $bank = Bank::create(Currency::EUR(), Currency::USD(), 1.2);
         $portfolio = new Portfolio($bank);
-        $portfolio->addMoney(Currency::USD(), 5);
-        $portfolio->addMoney(Currency::USD(), 5);
-        $portfolio->addMoney(Currency::EUR(), 10);
+        $money = new Money(5,Currency::USD());
+        $money3 = new Money(5,Currency::USD());
+        $money2 = new Money(10,Currency::EUR());
+        $portfolio->addMoney($money);
+        $portfolio->addMoney($money3);
+        $portfolio->addMoney($money2);
         $sum = $portfolio->sum(Currency::USD());
         
         $this->assertEquals(22, $sum);
