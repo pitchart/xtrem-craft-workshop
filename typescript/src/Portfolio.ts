8c78c03bc357fbed70e3a0a015096ba01affdba0
TripleLardon <bardinet.andrea@gmail.com>
2023-03-16 11:23:21 +0100

    push

--- typescript/src/Portfolio.ts
+++ typescript/src/Portfolio.ts
@@ -1,26 +1,42 @@
+import { Bank } from "./Bank";
 import { Currency } from "./Currency";
 
 
 interface Transaction {
-    currency : Currency,
-    amount : number 
+    currency: Currency,
+    amount: number
 }
 
 export class Portfolio {
 
-    private readonly _transactions: Array<Transaction>;
+    private _transactions: Array<Transaction>;
 
-    add(amount: number, currency: Currency){
-        if(amount === null){
-            throw new Error("Missing amount")
-        }
-        if(!currency){
-            throw new Error("Missing currency")
-        }
-        this._transactions.push()
+    constructor() {
+        this._transactions = [];
     }
 
-    value()
+
+    add(amount: number, currency: Currency) {
+        if (amount === null) {
+            throw new Error("Missing amount")
+        }
+        if (!currency) {
+            throw new Error("Missing currency")
+        }
+        let t: Transaction = {
+            currency: currency,
+            amount: amount
+        }
+
+        this._transactions.push(t)
+    }
+
+    value(currency: Currency, bank: Bank): number {
+        return this._transactions.reduce(
+            (accumulator, currentValue) => accumulator + bank.Convert(currentValue.amount, currentValue.currency, currency),
+            0
+        );
+    }
 
 
 
--- typescript/tests/Portfolio.spec.ts
+++ typescript/tests/Portfolio.spec.ts
@@ -1,20 +1,33 @@
+import { Bank } from "../src/Bank";
 import { Currency } from "../src/Currency";
+import { Portfolio } from "../src/Portfolio";
 
 describe('Portfolio', () => {
 
 
-    let portfolio;
+    let portfolio : Portfolio;
+    let bank : Bank;
 
     beforeEach(() => {
         portfolio = new Portfolio()
+        bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2)
+        bank.AddExchangeRate(Currency.EUR, Currency.KRW, 10 )
     })
 
 
     test("Add 5 USD and 10 EUR to return value 17 USD", () => {
         portfolio.add(10, Currency.EUR)
         portfolio.add(5, Currency.USD)
-        expect(portfolio.value(Currency.USD)).toBeNumber()
-        expect(portfolio.value(Currency.USD)).toBe(17)
+        expect(portfolio.value(Currency.USD, bank)).toBeNumber()
+        expect(portfolio.value(Currency.USD, bank)).toBe(17)
     })
+
+    test("Add 5 USD and 10 EUR to return value 18940W", () => {
+        portfolio.add(10, Currency.EUR)
+        portfolio.add(5, Currency.USD)
+        expect(portfolio.value(Currency.KRW, bank)).toBeNumber()
+        expect(portfolio.value(Currency.KRW, bank)).toBe(18940)
+    })
+
 
 })
