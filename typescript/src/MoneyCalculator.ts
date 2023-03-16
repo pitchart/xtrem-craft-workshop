bead431435f6f06257beeebe57ace5236de26545
Thomas <mail.thomasclement@gmail.com>
2023-03-16 15:47:08 +0100

    added money and tests

--- typescript/src/MoneyCalculator.ts
+++ typescript/src/MoneyCalculator.ts
@@ -1,7 +1,37 @@
-import { Currency } from './Currency'
+import { Currency } from './Currency';
+
+export class Money {
+	private amount: number;
+	private currency: Currency;
+
+	constructor(amount: number, currency: Currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
+	times(time: number): Money {
+		return new Money(this.amount * time, this.currency);
+	}
+
+	add(money: Money): Money {
+		if (this.currency !== money.currency)
+			throw new Error(
+				`Can not add ${this.currency} with ${money.currency}`
+			);
+
+		return new Money(this.amount + money.amount, this.currency);
+	}
+
+	divide(time: number): Money {
+		return new Money(this.amount / time, this.currency);
+	}
+}
 
 export const MoneyCalculator = {
-  Add: (amount: number, currency: Currency, amount2: number): number => amount + amount2,
-  Times: (amount: number, currency: Currency, number: number): number => amount * number,
-  Divide: (amount: number, currency: Currency, value: number): number => amount / value
-}
+	Add: (amount: number, currency: Currency, amount2: number): number =>
+		amount + amount2,
+	Times: (amount: number, currency: Currency, number: number): number =>
+		amount * number,
+	Divide: (amount: number, currency: Currency, value: number): number =>
+		amount / value,
+};
