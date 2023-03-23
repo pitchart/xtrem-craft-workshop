b2d94f111316d4c85caa58cea6200d4ae2cee2dc
AzGody <obastien4@gmail.com>
2023-03-23 14:46:47 +0100

    test1 Moneytest

--- java/src/test/java/money_problem/domain/MoneyTest.java
+++ java/src/test/java/money_problem/domain/MoneyTest.java
@@ -9,19 +9,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MoneyTest {
 
     static Bank bank = Bank.withExchangeRate(EUR, USD, 1.2);
+    
 
     @Test
     @DisplayName("5 EUR + 10 EUR = 15 EUR")
-    void shouldAdd1() {
-        //Arrange
-        final Portfolio portfolio = new Portfolio();
-        portfolio.add(5,EUR);
-        portfolio.add(10, EUR);
+    void shouldAdd1() throws Exception {
+        final Money money = new Money(5, EUR);
+        money.add(new Money(10, EUR));
 
-        //Act
-        final double result = portfolio.evaluate(EUR, bank);
-
-        //Assert
-        assertThat(result).isEqualTo(15);
+        assertThat(money.value).isEqualTo(15);
     }
 }
