464773bf33efea563a16d1b8d0eb8abcaa57370b
TDpy <toto.dupuy6558@gmail.com>
2023-03-23 14:42:41 +0100

    fix add and minus method
    -> it takes a Money as parameter, not int

--- java/src/main/java/money_problem/domain/Money.java
+++ java/src/main/java/money_problem/domain/Money.java
@@ -4,27 +4,37 @@ public class Money {
     int value;
     Currency currency;
 
-    public Money(int value, Currency currency){
+    public Money(int value, Currency currency) {
         this.value = value;
         this.currency = currency;
     }
 
-    public int add(int add){
-        this.value = this.value + add;
+    public int add(Money money) throws Exception {
+        if (money.currency.equals(this.currency)) {
+            this.value = this.value + money.value;
+            return this.value;
+        } else {
+            throw new Exception("Currency you tried to add aren't the same");
+        }
+    }
+
+    public int minus(Money money) throws Exception {
+        if (!money.currency.equals(this.currency)) {
+            throw new Exception("Currency you tried to remove aren't the same");
+        }
+        if (money.value > this.value) {
+            throw new Exception("The amout you tried to remove is too high");
+        }
+        this.value = this.value - money.value;
         return this.value;
     }
 
-    public int times(int times){
+    public int times(int times) {
         this.value = this.value * times;
         return this.value;
     }
 
-    public int minus(int minus){
-        this.value = this.value - minus;
-        return this.value;
-    }
-
-    public int divide(int divide){
+    public int divide(int divide) {
         this.value = this.value / divide;
         return this.value;
     }
