84464fedb843da32d0a5570088acbcd30a54ca56
TDpy <toto.dupuy6558@gmail.com>
2023-03-23 14:19:10 +0100

    adding Money class, with operations to add, multiply, divide, minus

---
+++ java/src/main/java/money_problem/domain/Money.java
@@ -0,0 +1,32 @@
+package money_problem.domain;
+
+public class Money {
+    int value;
+    Currency currency;
+
+    public Money(int value, Currency currency){
+        this.value = value;
+        this.currency = currency;
+    }
+
+    public int add(int add){
+        this.value = this.value + add;
+        return this.value;
+    }
+
+    public int times(int times){
+        this.value = this.value * times;
+        return this.value;
+    }
+
+    public int minus(int minus){
+        this.value = this.value - minus;
+        return this.value;
+    }
+
+    public int divide(int divide){
+        this.value = this.value / divide;
+        return this.value;
+    }
+
+}
