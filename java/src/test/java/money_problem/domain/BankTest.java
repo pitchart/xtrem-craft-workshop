a8f059efbd286e6881d171881c3f84c8bd7db7bb
dlevrault <damien.levrault@etu.u-bordeaux.fr>
2023-11-07 12:35:28 +0100

    tests pour le DataBuilder

--- java/src/test/java/money_problem/domain/BankTest.java
+++ java/src/test/java/money_problem/domain/BankTest.java
@@ -2,6 +2,8 @@ package money_problem.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static money_problem.domain.Currency.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -10,37 +12,38 @@ class BankTest {
 
     @Test
     void convert_eur_to_usd_test() throws MissingExchangeRateException {
-        double rate = 1.2;
-        Bank bank = Bank.withExchangeRate(EUR, USD, rate);
-        double convertedMoney = bank.convertMoney(new Money(EUR, 10), USD);
+        TestDataBuilderBank t = new TestDataBuilderBank();
+        HashMap<String, Double> exchangeRates = new HashMap<>();
+        exchangeRates.put("EUR->USD", 1.2);
+        Bank b = t.withExchangeRateBuilder(exchangeRates).build();
+        double convertedMoney = b.convertMoney(new Money(EUR, 10), USD);
         assertThat(convertedMoney).isEqualTo(12);
     }
 
-    /** Test pas pertinant
-    @Test
-    void convert_eur_to_eur_test() throws MissingExchangeRateException {
-        assertThat(Bank.withExchangeRate(EUR, USD, 1.2).convertMoney(10, EUR, EUR))
-                .isEqualTo(10);
-    }
-    */
-
     @Test
     void missing_exchange_rate_test() {
-        double rate = 1.2;
-        Bank bank = Bank.withExchangeRate(EUR, USD, rate);
-        assertThatThrownBy(() -> bank.convertMoney(new Money(EUR, 10), KRW))
+        TestDataBuilderBank t = new TestDataBuilderBank();
+        HashMap<String, Double> exchangeRates = new HashMap<>();
+        exchangeRates.put("EUR->USD", 1.2);
+        Bank b = t.withExchangeRateBuilder(exchangeRates).build();
+        assertThatThrownBy(() -> b.convertMoney(new Money(EUR, 10), KRW))
                 .isInstanceOf(MissingExchangeRateException.class)
                 .hasMessage("EUR->KRW");
     }
 
     @Test
     void different_exchange_rates_test() throws MissingExchangeRateException {
-        Bank firstBank = Bank.withExchangeRate(EUR, USD, 1.2);
-        double firstConvertedMoney = firstBank.convertMoney(new Money(EUR, 10), USD);
+        TestDataBuilderBank t = new TestDataBuilderBank();
+        HashMap<String, Double> exchangeRates1 = new HashMap<>();
+        HashMap<String, Double> exchangeRates2 = new HashMap<>();
+        exchangeRates1.put("EUR->USD", 1.2);
+        exchangeRates2.put("EUR->USD", 1.3);
+        Bank b = t.withExchangeRateBuilder(exchangeRates1).build();
+        double firstConvertedMoney = b.convertMoney(new Money(EUR, 10), USD);
         assertThat(firstConvertedMoney).isEqualTo(12);
 
-        Bank secondBank = Bank.withExchangeRate(EUR, USD, 1.3);
-        double secondConvertedMoney = secondBank.convertMoney(new Money(EUR, 10), USD);
+        Bank b2 = t.withExchangeRateBuilder(exchangeRates2).build();
+        double secondConvertedMoney = b2.convertMoney(new Money(EUR, 10), USD);
         assertThat(secondConvertedMoney).isEqualTo(13);
     }
 }
