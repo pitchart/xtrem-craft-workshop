e29773216285be084ac264b00aafb01e440c8fe0
gdumarchat <gurvandumarchat@gmail.com>
2026-03-12 13:08:34 +0100

    step-06

---
+++ typescript/src/MissingExchangeRateError.ts
@@ -0,0 +1,7 @@
+import { Currency } from './Currency';
+
+export class MissingExchangeRateError extends Error {
+  constructor(currency1: Currency, currency2: Currency) {
+    super(currency1 + '-> ' + currency2);
+  }
+}
